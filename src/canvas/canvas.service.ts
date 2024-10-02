94fb7d7306ed138311b47e607888e8e3a4df0eee
ezrasujeevan <ezra.sujeevan@gmail.com>
2024-10-02 11:01:18 +0530

    feat: Update canvas service to display long text
    Update the canvas service in order to display a long text on the canvas. The canvas service now includes a new property called "longText" which contains the text to be displayed. The text is then rendered on the canvas using the fillText method. The alignment and position of the text have also been adjusted to ensure proper display.

--- src/canvas/canvas.service.ts
+++ src/canvas/canvas.service.ts
@@ -4,6 +4,8 @@ import { Canvas, createCanvas } from 'canvas';
 @Injectable({ scope: Scope.DEFAULT })
 export class CanvasService {
   private canvas: Canvas;
+  private readonly longText =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
   private readonly width = 1056;
   private readonly height = 816;
   constructor() {
@@ -18,10 +20,11 @@ export class CanvasService {
     ctx.font = '48px serif';
     ctx.textAlign = 'left';
     ctx.fillStyle = 'black';
-    ctx.fillText('Hello World!\n line 1', this.width / 2, this.height / 2);
+    console.log(ctx.measureText(this.longText));
+    ctx.fillText(this.longText, 0, this.height / 2);
     ctx.fillStyle = 'red';
-    ctx.textAlign = 'start';
-    ctx.fillText('Hello World! \n line 2', this.width / 2, this.height / 2, 10);
+    ctx.textAlign = 'left';
+    ctx.fillText('Hello World!', 0, 48, this.width / 2);
     const buffer = this.canvas.toDataURL('image/jpeg');
     return { data: buffer };
   }
